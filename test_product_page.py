f3a5882c441400dcdb56beda8a64148e855e8ee9
rtl-ylnomoc <lokate52@gmail.com>
2020-08-02 22:44:55 +0500

    deleted garbage

--- test_product_page.py
+++ test_product_page.py
@@ -53,6 +53,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_right_basket_total()
 
 
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, browser.current_url)
+    page.should_not_be_products_in_basket()
+    page.should_be_basket_empty_text()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -83,22 +102,3 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page = BasketPage(browser, browser.current_url)
-    page.should_not_be_products_in_basket()
-    page.should_be_basket_empty_text()
